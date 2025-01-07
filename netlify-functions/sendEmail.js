9b33fb4c590250eeda5daf952473a69a911343ab
srid3 <venkateshwari.amul@gmail.com>
2025-01-08 01:54:40 +0530

    Enable Notification to mail

---
+++ netlify-functions/sendEmail.js
@@ -0,0 +1,40 @@
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'colourtek07@gmail.com',
+    pass: 'Q1kd6acwwH2vyKz',
+  },
+});
+
+exports.handler = async (event, context) => {
+  if (event.httpMethod !== 'POST') {
+    return {
+      statusCode: 405,
+      body: JSON.stringify({ message: 'Method Not Allowed' }),
+    };
+  }
+
+  const { name, email, subject, message } = JSON.parse(event.body);
+
+  const mailOptions = {
+    from: email,
+    to: 'colourtek07@gmail.com',
+    subject: `New Message from ${name}: ${subject}`,
+    text: `Message: ${message}\nFrom: ${name}\nEmail: ${email}`,
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: 'Email sent successfully!' }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: 'Failed to send email', error: error.message }),
+    };
+  }
+};
