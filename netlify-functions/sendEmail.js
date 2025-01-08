44a0f4715161d37ca16296deb2fd32ac118b0e68
srid3 <venkateshwari.amul@gmail.com>
2025-01-08 18:45:00 +0530

    Add .env file

--- netlify-functions/sendEmail.js
+++ netlify-functions/sendEmail.js
@@ -3,8 +3,8 @@ import nodemailer from 'nodemailer';
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: 'colourtek07@gmail.com',
-    pass: 'vlox ktpe xmqq pqee',
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
   },
 });
 
